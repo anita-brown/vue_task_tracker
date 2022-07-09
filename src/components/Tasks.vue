9fe7c5af91c4447c9f59c06ccbc113f845c78b49
Anita <Efosa@Anita-Efosa.local>
2022-07-09 23:14:59 +0100

    task component created

---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :key="task.id " v-for="task in tasks">
+   <Task :task="task"/>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Tasks',
+  components: { Task },
+  props: {
+    tasks: Array
+  }
+}
+</script>
+
+<style></style>
