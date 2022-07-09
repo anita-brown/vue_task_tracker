9e1de9eae6a7eb4ba9e1afbc5748f460915e2ae5
Anita <Efosa@Anita-Efosa.local>
2022-07-09 23:46:27 +0100

    fix: delete task functionality implemented

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="task">
-    <h3>{{ task.text }}</h3>
-    <p>{{task.day}}</p>
+  <div @dblclick="$emit('toggle-reminder')" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <i @click="$emit('delete-task', task.id )" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
   </div>
 </template>
 <script>
@@ -14,6 +17,9 @@ export default {
 }
 </script>
 <style scoped>
+.fas {
+  color: red;
+}
 .task {
   background: #f4f4f4;
   margin: 5px;
