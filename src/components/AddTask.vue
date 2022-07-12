8b196ce41ebf651e519b2c7fd78c3b868e3e0e31
Anita <Efosa@Anita-Efosa.local>
2022-07-12 01:04:30 +0100

    completed

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -28,29 +28,33 @@ export default {
   data () {
     return {
       text: '',
-      day: ' ',
+      day: '',
       reminder: false
     }
   },
   methods: {
     onSubmit (e) {
       e.preventDefault()
+
       if (!this.text) {
         alert('Please add a task')
+        return
       }
+
       const newTask = {
-        id: Math.floor(Math.random() * 10000),
+        // id: Math.floor(Math.random() * 100000),
         text: this.text,
         day: this.day,
         reminder: this.reminder
       }
+
       this.$emit('add-task', newTask)
 
       this.text = ''
       this.day = ''
+      this.reminder = false
     }
   }
-
 }
 </script>
 
